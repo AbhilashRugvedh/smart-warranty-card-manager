0ba5f62007e2ccea03066dec87d26359f4831ee8
AbhilashJangili <165947272+AbhilashRugvedh@users.noreply.github.com>
2025-07-23 17:27:00 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -14,6 +15,22 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/warranty", warrantyRoutes);
+
+// ----------------- Deployment config -----------------
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+// ------------------------------------------------------
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
