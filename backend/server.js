020894cdd8ec41a5b11a8be15693be7f91e228cd
AbhilashJangili <165947272+AbhilashRugvedh@users.noreply.github.com>
2025-07-23 17:31:44 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from "path";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -16,21 +15,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/warranty", warrantyRoutes);
 
-// ----------------- Deployment config -----------------
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
-// ------------------------------------------------------
+app.get("/", (req, res) => {
+  res.send("Smart Warranty Card API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
